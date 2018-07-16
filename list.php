2d3bac967893cd5f601bc9252c91c5355e33afc7
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-07-16 23:22:22 +1000

    Update list.php

--- list.php
+++ list.php
@@ -9,16 +9,23 @@
 </html>
 <?php
 if (!empty($_FILES)) {
-	echo "<pre>";
-	print_r($_FILES['tests'] ['name']);
-	echo "<pre>";
+	/*echo "<pre>";
+	print_r($_FILES);
+	echo "<pre>";*/
 }
+if (move_uploaded_file($_FILES['tests'] ['tmp_name'] ['0'], 'exams/testss1.json') && move_uploaded_file($_FILES['tests'] ['tmp_name'] ['1'], 'exams/testss2.json') && move_uploaded_file($_FILES['tests'] ['tmp_name'] ['2'], 'exams/testss3.json')) {
+	echo 'Файлы успешно загружены, выбирайте тест =)';
+} else{
+	echo 'К сожалению файлы не загружены, повторите загрузку';
+}
+
 ?>
-<form action="test.php>" method="GET">
-	<?php 
-	for ($i=0; $i<3 ; $i++) { ?> 
-		<label><input type="radio" name="q1"><?php print_r($_FILES['tests'] ['name'] [$i]); ?> </label>
+
+	<?php for ($i=0; $i<3; $i++) {?> 
+		<p> <?php print_r($_FILES['tests'] ['name'] [$i]);?></p>
 	<?php } ?>
 
-  <input type="submit" value="Отправить">  
-</form>
+	<form action="test.php>" method="GET">
+	<input type="text" name="number" placeholder="Введите номер теста">
+  	<input type="submit" value="Отправить">  
+	</form>
