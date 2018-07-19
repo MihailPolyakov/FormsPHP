e82450257f620354e5d97ed89930164d97d224f9
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-07-20 09:32:56 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,57 +1,37 @@
-<?php
-	if ($_GET['number']==1) {
-		echo'Выводим первый тест';
-		$test = file_get_contents("exams/testss1.json");
-		$array =json_decode($test);?>
+	<form action="" method="GET">
+		<div><input type="text" name="number" placeholder="Введите номер"></div>
+		<input type="submit" value="Отправить">
+	</form>     
+
+<?php	if (!empty($_GET)) {
+			$array=[];
+			$test = 0;
+			$files='C:/xampp/htdocs/example/exams';
+			$arrayfiles=scandir($files);
+			shuffle($arrayfiles);
+			foreach ($arrayfiles as $value) {
+				if ($value == '.' || $value == '..'){
+					continue;
+				}
+				$test = file_get_contents("exams/$value");
+				break;
+			}	
+			$array =json_decode($test);
+			$qcount = 0;?>
+		
 		<form action="" method="POST">
 
-        <?php foreach ($array as $key => $value) {?>
-          <fieldset>
-            <legend> <?php echo $key ?></legend>
-            <?php foreach ($value as $asks) {?>
-            <label><input type="radio" name="q1"> <?php echo $asks ?> </label>  
-            <?php } ?>
-          </fieldset> 
-        <?php } ?>
-        <input type="submit" value="Отправить">  
-      </form>
-	
-	<?php } elseif ($_GET['number']==2) {
-		echo'Выводим второй тест';
-		$test = file_get_contents("exams/testss2.json");
-		$array =json_decode($test);?>
-		<form action="" method="POST">
-
-        <?php foreach ($array as $key => $value) {?>
-          <fieldset>
-            <legend> <?php echo $key ?></legend>
-            <?php foreach ($value as $asks) {?>
-            <label><input type="radio" name="q1"> <?php echo $asks ?> </label>  
-            <?php } ?>
-          </fieldset> 
-        <?php } ?>
-        <input type="submit" value="Отправить">  
-      </form>
-	<?php 
-	} elseif ($_GET['number']==3) {
-		echo'Выводим Третий тест';
-		$test = file_get_contents("exams/testss3.json");
-		$array =json_decode($test);?>
-		<form action="" method="POST">
-
-        <?php foreach ($array as $key => $value) {?>
-          <fieldset>
-            <legend> <?php echo $key ?></legend>
-            <?php foreach ($value as $asks) {?>
-            <label><input type="radio" name="q1"> <?php echo $asks ?> </label>  
-            <?php } ?>
-          </fieldset> 
-        <?php } ?>
-        <input type="submit" value="Отправить">  
-      </form>
-	<?php } else {
-		echo'Извините, но такого номера теста не существует';
-	}
-
+		<?php foreach ($array as $key => $value) {
+			++$qcount;?>	
+		  <fieldset>
+		    <legend> <?php echo $key ?></legend>
+			<?php foreach ($value as $asks) {?>
+		    <label><input type="radio" name="q<?php echo $qcount?>"> <?php echo $asks ?> </label>  
+			      <?php } ?>
+		  </fieldset> 
+		      <?php } ?>
+		<input type="submit" value="Отправить">  
+		</form>
+      	<?php } ?>
 	var_dump($_POST);
 
