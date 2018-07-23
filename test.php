b3314685b48b525fa482d3e27f97b5fcdc869f9e
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-07-23 16:09:58 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,37 +1,29 @@
-	<form action="" method="GET">
-		<div><input type="text" name="number" placeholder="Введите номер"></div>
-		<input type="submit" value="Отправить">
-	</form>     
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+</head>
+<body>
+	
+<form enctype="multipart/form-data" action=" " method="POST">
+	<!--<div><input type="text" name="Введите число"></div>-->
+	<div><input type="file" name="tests[]"></div>
+	<div><input type="submit" value="Отправить"></div>
+</form>
 
-<?php	if (!empty($_GET)) {
-			$array=[];
-			$test = 0;
-			$files='C:/xampp/htdocs/example/exams';
-			$arrayfiles=scandir($files);
-			shuffle($arrayfiles);
-			foreach ($arrayfiles as $value) {
-				if ($value == '.' || $value == '..'){
-					continue;
-				}
-				$test = file_get_contents("exams/$value");
-				break;
+<?php if (!empty($_FILES)) {
+		$files='C:/xampp/htdocs/example/exams';
+		$arrayfiles=scandir($files);
+		foreach ($arrayfiles as $value) {
+			if ($value == $_FILES['tests'] ['name'] ['0']) {
+				echo "Такое имя уже существует";
+			}else{
+				$newfl = 'exams/' . $_FILES['tests'] ['name'] ['0'];
+				move_uploaded_file($_FILES['tests'] ['tmp_name'] ['0'], $newfl);
 			}	
-			$array =json_decode($test);
-			$qcount = 0;?>
-		
-		<form action="" method="POST">
-
-		<?php foreach ($array as $key => $value) {
-			++$qcount;?>	
-		  <fieldset>
-		    <legend> <?php echo $key ?></legend>
-			<?php foreach ($value as $asks) {?>
-		    <label><input type="radio" name="q<?php echo $qcount?>"> <?php echo $asks ?> </label>  
-			      <?php } ?>
-		  </fieldset> 
-		      <?php } ?>
-		<input type="submit" value="Отправить">  
-		</form>
-      	<?php } ?>
-	var_dump($_POST);
-
+		}	
+} else{
+	echo 'К сожалению файлы не загружены, повторите загрузку';
+}?>
+</body>
+</html>
