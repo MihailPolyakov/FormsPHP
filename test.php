588ef9ec051b795a2c258ec76ae9d3150b295928
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-07-23 16:11:28 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,29 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Admin</title>
-</head>
-<body>
-	
-<form enctype="multipart/form-data" action=" " method="POST">
-	<!--<div><input type="text" name="Введите число"></div>-->
-	<div><input type="file" name="tests[]"></div>
-	<div><input type="submit" value="Отправить"></div>
-</form>
+<?php	if (!empty($_GET)) {
+			$array=[];
+			$test = 0;
+			$int =0;
+			$fl=0;
+			$files='C:/xampp/htdocs/example/exams';
+			$arrayfiles=scandir($files, 1);
+			if (array_key_exists($_GET['number'], $arrayfiles) == true) {
+				$int = $_GET['number'];
+				if ($arrayfiles[$int]=='.' || $arrayfiles[$int]== '..'){
+					echo 'такого номера не существует';
+				} else {
+					$fl=$arrayfiles[$int];
+					$test = file_get_contents("exams/$fl");
+					$array =json_decode($test);
+					$qcount = 0;
+					?>
 
-<?php if (!empty($_FILES)) {
-		$files='C:/xampp/htdocs/example/exams';
-		$arrayfiles=scandir($files);
-		foreach ($arrayfiles as $value) {
-			if ($value == $_FILES['tests'] ['name'] ['0']) {
-				echo "Такое имя уже существует";
-			}else{
-				$newfl = 'exams/' . $_FILES['tests'] ['name'] ['0'];
-				move_uploaded_file($_FILES['tests'] ['tmp_name'] ['0'], $newfl);
-			}	
-		}	
-} else{
-	echo 'К сожалению файлы не загружены, повторите загрузку';
-}?>
-</body>
-</html>
+					<form action="" method="POST">
+        			<?php 
+			        foreach ($array as $key => $value) {
+			        	
+			        
+				        foreach ($array as $key => $value) {
+				        	++$qcount;
+				        	$i = 0;
+				        	?>
+				          <fieldset>
+				            <legend> <?php echo $key ?></legend>
+				            <?php foreach ($value as $asks) {
+				            	  ++$i; ?>
+				            <label><input type="radio" name="q<?php echo $qcount?>" value = "<?php echo $i?>"> <?php echo $asks ?></label>  
+				            <?php }?>
+				          </fieldset> 
+				        <?php }
+				    }    
+			    }?>
+			          <input type="submit" value="Отправить">  
+			      	</form>
+				
+	   <?php } else {
+				echo 'Такого номера теста не существует';
+			}
+		};	
+	echo "<pre>";	
+	print_r($_POST);
+	echo "</pre>";
